c0f355c7ad4c2a4cdb7e34d258ed05773f979f92
Abdullah <abdullahalbattat@outlook.com>
2025-12-03 17:42:37 +0300

    Revert "Revert "Implement DB (SQLite persistence layer (US9, US10))""

--- app/src/main/java/com/example/boki/MainActivity.java
+++ app/src/main/java/com/example/boki/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.boki;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
