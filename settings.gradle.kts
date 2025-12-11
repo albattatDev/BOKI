73cb8532dd70e1a95a36e70f8d86c2258c4913ea
abdullah <abdullahalbattat@outlook.com>
2025-12-11 22:40:11 +0300

    done part of expenses fragmetn design

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,9 +16,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") } // Corrected syntax for maven url
     }
 }
 
 rootProject.name = "BOKI"
 include(":app")
- 
